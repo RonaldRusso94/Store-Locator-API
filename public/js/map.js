5f944acc9ee066b61b66993a5abdd79f33f1e834
Ronald Russo <RonaldRusso94@yahoo.com>
2020-03-24 17:28:41 -0400

    Update map.js

--- public/js/map.js
+++ public/js/map.js
@@ -4,7 +4,7 @@ const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
   zoom: 9,
-  center: [-71.157895, 42.707741]
+  center: [-73.887199, 40.701401]
 });
 
 // Fetch stores from API
